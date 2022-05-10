1f498ff3585838d4bc8eccca88ccb4089eae1298
unknown <phuonglewlew2020@gmail.com>
2022-05-10 10:55:41 +0700

    Taco -back

---
+++ src/test/java/Web/tacocloudapibackend/TacoCloudApiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Web.tacocloudapibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
